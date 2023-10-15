6d7f948d7d3217c3daf0ad1d4aa21b00859e409e
Gantavya Saraswat <gantavya99@gmail.com>
2023-10-16 01:18:52 +0530

    Carousel basic complete

--- minipod/src/components/Carousel/Carousel.jsx
+++ minipod/src/components/Carousel/Carousel.jsx
@@ -8,22 +8,46 @@ import img5 from "../../assets/Car5.jpg";
 
 export const Carousel = () => {
   const slides = [img1, img2, img3, img4, img5];
-  const [currIndex, setCurrIndex] = useState(0);
-  const [leftImgIndex, setLeftImgIndex] = useState(4); // Initialize with the last image
-  const [rightImgIndex, setRightImgIndex] = useState(1); // Initialize with the second image
-
-  const nextSlide = () => {
-    const nextIndex = currIndex === slides.length - 1 ? 0 : currIndex + 1;
-    setLeftImgIndex(currIndex);
-    setCurrIndex(nextIndex);
-    setRightImgIndex(nextIndex === slides.length - 1 ? 0 : nextIndex + 1);
-  };
-
+  const [leftImg1Index, setLeftImg1Index] = useState(0);
+  const [leftImgIndex, setLeftImgIndex] = useState(1);
+  const [currIndex, setCurrIndex] = useState(2);
+  const [rightImgIndex, setRightImgIndex] = useState(3);
+  const [rightImg1Index, setRightImg1Index] = useState(4);
   const prevSlide = () => {
-    const prevIndex = currIndex === 0 ? slides.length - 1 : currIndex - 1;
-    setLeftImgIndex(prevIndex === 0 ? slides.length - 1 : prevIndex - 1);
-    setCurrIndex(prevIndex);
-    setRightImgIndex(currIndex);
+    const nextSlide = currIndex === slides.length - 1 ? 0 : currIndex + 1;
+    const nextLeftSlide =
+      leftImgIndex === slides.length - 1 ? 0 : leftImgIndex + 1;
+    const nextRightSlide =
+      rightImgIndex === slides.length - 1 ? 0 : rightImgIndex + 1;
+    const nextRight1Slide =
+      rightImg1Index === slides.length - 1 ? 0 : rightImg1Index + 1;
+    const nextLeft1Slide =
+      leftImg1Index === slides.length - 1 ? 0 : leftImg1Index + 1;
+    setCurrIndex(nextSlide);
+    setLeftImgIndex(nextLeftSlide);
+    setRightImgIndex(nextRightSlide);
+    setLeftImg1Index(nextLeft1Slide);
+    setRightImg1Index(nextRight1Slide);
+    console.log(`leftIndex=${leftImgIndex}`);
+    console.log(`currIndex=${currIndex}`);
+    console.log(`rightIndex=${rightImgIndex}`);
+  };
+  const nextSlide = () => {
+    const nextSlide = currIndex === 0 ? slides.length - 1 : currIndex - 1;
+    const nextLeftSlide =
+      leftImgIndex === 0 ? slides.length - 1 : leftImgIndex - 1;
+    const nextRightSlide =
+      rightImgIndex === 0 ? slides.length - 1 : rightImgIndex - 1;
+    const nextRight1Slide =
+      rightImg1Index === 0 ? slides.length - 1 : rightImg1Index - 1;
+    const nextLeft1Slide =
+      leftImg1Index === 0 ? slides.length - 1 : leftImg1Index - 1;
+    setCurrIndex(nextSlide);
+    setLeftImgIndex(nextLeftSlide);
+    setRightImgIndex(nextRightSlide);
+    setLeftImg1Index(nextLeft1Slide);
+    setRightImg1Index(nextRight1Slide);
+    setCurrIndex(nextSlide);
   };
   const goToSlide = (slideIndex) => {
     setCurrIndex(slideIndex);
@@ -38,27 +62,41 @@ export const Carousel = () => {
     width: "600px", // Adjust the width as needed
     height: "400px", // Adjust the height as needed
     borderRadius: "20px",
-    zIndex:"9999"
+    zIndex: "9999",
   };
   const leftSideImg = {
-    backgroundImage: `url(${
-      slides[leftImgIndex === 0 ? slides.length - 1 : leftImgIndex - 1]
-    })`,
+    backgroundImage: `url(${slides[leftImgIndex]})`,
     backgroundSize: "cover", // Optional: Adjust background size
     width: "450px", // Adjust the width as needed
     height: "300px",
     borderRadius: "20px", // Position the left image absolutely
-    marginRight:"-250px"
+    marginRight: "-250px",
+    zIndex: "10",
+  };
+  const leftSideImg1 = {
+    backgroundImage: `url(${slides[leftImg1Index]})`,
+    backgroundSize: "cover", // Optional: Adjust background size
+    width: "300px", // Adjust the width as needed
+    height: "200px",
+    borderRadius: "20px", // Position the left image absolutely
+    marginRight: "-150px",
   };
   const rightSideImg = {
-    backgroundImage: `url(${
-      slides[rightImgIndex === slides.length - 1 ? 0 : rightImgIndex + 1]
-    })`,
+    backgroundImage: `url(${slides[rightImgIndex]})`,
     backgroundSize: "cover", // Optional: Adjust background size
     width: "450px", // Adjust the width as needed
     height: "300px", // Adjust the height as needed
     borderRadius: "20px",
-    marginLeft:"-250px"
+    marginLeft: "-250px",
+    zIndex: "10",
+  };
+  const rightSideImg1 = {
+    backgroundImage: `url(${slides[rightImg1Index]})`,
+    backgroundSize: "cover", // Optional: Adjust background size
+    width: "300px", // Adjust the width as needed
+    height: "200px", // Adjust the height as needed
+    borderRadius: "20px",
+    marginLeft: "-150px",
   };
 
   const rightArrowStyles = {
@@ -96,14 +134,16 @@ export const Carousel = () => {
     <div className="container">
       <h1>Carousel</h1>
       <div style={sliderContainer}>
+        <div style={leftSideImg1}></div>
         <div style={leftSideImg}></div>
         <div style={mainImg}></div>
         <div style={rightSideImg}></div>
+        <div style={rightSideImg1}></div>
       </div>
 
       <div></div>
       <div style={buttonContainer}>
-        <div onClick={(index) => prevSlide(index)} style={leftArrowStyles}>
+        <div onClick={() => prevSlide()} style={leftArrowStyles}>
           ❰
         </div>
         <div style={dotsContainerStyles}>
@@ -118,7 +158,7 @@ export const Carousel = () => {
             </div>
           ))}
         </div>
-        <div onClick={(index) => nextSlide(index)} style={rightArrowStyles}>
+        <div onClick={() => nextSlide()} style={rightArrowStyles}>
           ❱
         </div>
       </div>
